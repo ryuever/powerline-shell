4480edc55bafa66cd9a3332fe7dbef9a81afe5af
ryu <ryuforever@163.com>
2015-11-30 23:31:01 +0800

    At most five items on the shell prompt

--- powerline-shell.py
+++ powerline-shell.py
@@ -67,8 +67,21 @@ class Powerline:
             separator_fg if separator_fg is not None else bg))
 
     def draw(self):
-        return (''.join(self.draw_segment(i) for i in range(len(self.segments)))
-                + self.reset).encode('utf-8') + ' '
+        # return (''.join(self.draw_segment(i) for i in range(len(self.segments)))
+        #         + self.reset).encode('utf-8') + ' '
+        # sys.stdout.write('length' + str(len(self.segments)))
+        if len(self.segments) > 5:
+            #sys.stdout.write(str(len(self.segments)))
+            limit = range(len(self.segments)-1, len(self.segments)-5, -1)
+            limit.reverse()
+            #limit = range(3, len(self.segments))
+            limit.insert(0, 1)
+            # reversed((range(len(self.segments)-1, 4, -1)).append(0)))
+            return (''.join(self.draw_segment(i) for i in limit)
+                    + self.reset).encode('utf-8') + ' '
+        else:
+            return (''.join(self.draw_segment(i) for i in range(1, len(self.segments)))
+                    + self.reset).encode('utf-8') + ' '
 
     def draw_segment(self, idx):
         segment = self.segments[idx]
@@ -302,32 +315,6 @@ def add_username_segment():
     powerline.append(user_prompt, Color.USERNAME_FG, bgcolor)
 
 add_username_segment()
-
-
-def add_hostname_segment():
-    if powerline.args.colorize_hostname:
-        from lib.color_compliment import stringToHashToColorAndOpposite
-        from lib.colortrans import rgb2short
-        from socket import gethostname
-        hostname = gethostname()
-        FG, BG = stringToHashToColorAndOpposite(hostname)
-        FG, BG = (rgb2short(*color) for color in [FG, BG])
-        host_prompt = ' %s ' % hostname.split('.')[0]
-
-        powerline.append(host_prompt, FG, BG)
-    else:
-        if powerline.args.shell == 'bash':
-            host_prompt = ' \\h '
-        elif powerline.args.shell == 'zsh':
-            host_prompt = ' %m '
-        else:
-            import socket
-            host_prompt = ' %s ' % socket.gethostname().split('.')[0]
-
-        powerline.append(host_prompt, Color.HOSTNAME_FG, Color.HOSTNAME_BG)
-
-
-add_hostname_segment()
 
 
 import os
